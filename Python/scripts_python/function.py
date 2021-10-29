13c26ce2eaa993fd3ea77df2b540bd71458430d4
jcuaran7 <60941660+jcuaran7@users.noreply.github.com>
2021-10-29 13:29:27 -0500

    atari
    atari

---
+++ Python/scripts_python/function.py
@@ -0,0 +1,9 @@
+def sumar(x = 0, y = 0):
+    sum = x+y
+    return sum
+
+a = 12
+b = 8
+
+print ("la suma es ",sumar(a,b))
+
---
+++ Unity/2d/My project2d/Assets/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string player_name = "Jeferson";
+        Debug.Log(player_name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string player_name2 = "Cuaran";
+        Debug.Log(player_name2);
+    }
+}
